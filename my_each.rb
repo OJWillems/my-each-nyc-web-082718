b6ddc299b927698e0cfc9a6e7205f3cfd777811c
Olivier Willems <ojwillems@gmail.com>
2018-08-22 19:39:11 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,11 @@
-def my_each(argument)
-  
+def my_each(array)
+  if block_given?
+    n = 0
+    while n < array.length
+      yield array[0]
+      n += 1
+    end
+    array
+  else
+    puts ""
 end
